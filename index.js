4666018cb54a8f6ec71212f3765ced4dff280d72
andrewhunt21 <andrewhunt21@icloud.com>
2021-08-16 13:08:48 -0700

    finished task 2

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
   
   2. Which of the two uses a closure? How can you tell?
   
@@ -62,8 +63,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random() * Math.floor(3));
 }
 
 
@@ -80,7 +81,7 @@ Use the finalScore function below to do the following:
   "Away": 5
 }
 */ 
-
+// params inningcb, number of innings to be played
 function finalScore(/*code Here*/){
   /*Code Here*/
 }
@@ -90,6 +91,8 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
+  // inningcb
+  // return an object with a score for home and away
 function getInningScore(/*Your Code Here */) {
   /*Your Code Here */
 }
@@ -136,6 +139,7 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
+// get inningscorecb, scorecb, number
 function scoreboard(/* CODE HERE */) {
   /* CODE HERE */
 }
